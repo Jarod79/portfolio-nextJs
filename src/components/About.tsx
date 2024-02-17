34cc197100b9f99e5d939a31af5901eec51d5003
eric arrijuria <damne2@hotmail.com>
2024-02-17 23:23:29 +0100

    Debug lint error

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,11 +3,14 @@ const About = () => {
   return (
     <div className="about">
       <p>
-        Fan de nouvelles technologies, et aimant apprendre, j’ai décidé de faire
+        Fan de nouvelles technologies, et aimant apprendre, j&apos;ai décidé de faire
         de ma passion mon métier. Ayant toujours été attiré par le métier de
-        développeur web, et après une vingtaine d’années riches en diverses
-        expériences professionnelles, j’ai décidé de mettre à profit mes
-        compétences acquises pour me lancer dans le numérique. Après avoir suivi la formation de Développeur web junior à la Wild Code School à Biarritz, me voilà aujourd'hui développeur web.
+        développeur web, et après une vingtaine d&apos;années riches en diverses
+        expériences professionnelles, j&apos;ai mis à profit mes
+        compétences acquises pour me lancer dans le numérique.
+        Après avoir suivi la formation de Développeur web junior 
+        à la Wild Code School à Biarritz, 
+        me voilà aujourd&apos;hui développeur web.
       </p>
     </div>
   );
